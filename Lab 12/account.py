e2491720128918ad5fc01102c6e55668cfd56e12
HarborUser <hareshjayaraman21@gmail.com>
2022-11-18 20:11:07 -0600

    added changes to account.py added a new file test_account.py

--- Lab 12/account.py
+++ Lab 12/account.py
@@ -1,27 +1,50 @@
-class Account():
-    def __init__(self,name):
+class Account:
+    def __init__(self, name: str) -> None:
+        """
+        This method is to initialize the values
+        :param name: The name of the account
+        """
 
-        self.__account_name=name
-        self.__account_balance=0
+        self.__account_name = name
+        self.__account_balance = 0
 
-    def deposit(self, amount):
-        if amount<0 or amount == 0:
+    def deposit(self, amount: float) -> bool:
+        """
+        This method deposits funds into a account
+        :param amount: Amount to be deposited
+        :return: False if the amount is not successful and True if it is successful
+        """
+
+        if amount < 0 or amount == 0:
             return False
-        if amount>0
+        else:
             self.__account_balance += amount
             return True
 
-    def withdrew(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        """
+        This method withdraws funds from the account
+        :param amount: Amount to be withdraw
+        :return: False if the amount is not successful and True if it is successful
+        """
         if amount < 0 or amount == 0 or amount > self.__account_balance:
             return False
-        if amount >0:
+        else:
             self.__account_balance -= amount
             return True
 
-
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        This method returns the account balance
+        :return: It returns account balance
+        """
         return self.__account_balance
-    def get_name(self):
+
+    def get_name(self) -> str:
+        """
+        This method return the name of the account
+        :return: It returns name of the account
+        """
         return self.__account_name
 
 
--- Lab 12/test_account.py
+++ Lab 12/test_account.py
@@ -1,8 +1,42 @@
+import pytest
 from account import *
 
+
 class Test:
-    def
 
+    def setup_method(self):
+        self.a1 = Account("001-John")
 
+    def teardown_method(self):
+        del self.a1
 
+    def test_init(self):
+        assert self.a1.get_name() == "001-John"
+        assert self.a1.get_balance() == 0
 
+    def test_deposit(self):
+        assert self.a1.deposit(5) is True
+        assert self.a1.get_balance() == 5
+
+        assert self.a1.deposit(-5) is False
+        assert self.a1.get_balance() == 5
+
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance() == 5
+
+        assert self.a1.deposit(5.2) is True
+        assert self.a1.get_balance() == pytest.approx(10.2, abs=0.001)
+
+    def test_withdraw(self):
+        self.a1.deposit(10)
+        assert self.a1.withdraw(2) is True
+        assert self.a1.get_balance() == 8
+
+        assert self.a1.withdraw(10) is False
+        assert self.a1.get_balance() == 8
+
+        assert self.a1.withdraw(-5) is False
+        assert self.a1.get_balance() == 8
+
+        assert self.a1.withdraw(0) is False
+        assert self.a1.get_balance() == 8
